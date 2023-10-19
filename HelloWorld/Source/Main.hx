614567eac56b66eb71d81f6a67353741560a09e1
Rainy <939029835@qq.com>
2023-10-19 23:59:58 +0800

    新增Hello World例子

---
+++ HelloWorld/Source/Main.hx
@@ -0,0 +1,24 @@
+package;
+
+import zygame.components.ZLabel;
+import zygame.core.Start;
+
+class Main extends Start {
+	public function new() {
+		super(1920, 1080, false);
+	}
+
+	override function onInit() {
+		super.onInit();
+		// 代码初始化入口
+		var text = new ZLabel();
+		text.dataProvider = "Hello World";
+		text.setFontSize(60);
+		text.hAlign = CENTER;
+		text.vAlign = CENTER;
+		text.width = 500;
+		this.addChild(text);
+		text.x = (getStageWidth() - text.width) / 2;
+		text.y = (getStageHeight() - text.height) / 2;
+	}
+}
